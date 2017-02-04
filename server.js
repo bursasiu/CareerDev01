8fbc1b6c2b782c4c96f7b381cb631732975067cb
bursasiu <bursasiu@gmail.com>
2017-02-04 18:21:30 +0200

    comit02

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, Career dev!');
+    res.end('Hello, Career dev02!');
     
 }).listen(process.env.PORT || 8080);
